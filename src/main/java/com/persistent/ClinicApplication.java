b26004afa64a1e3716f6eeb2dd3b939bf2c3cdd2
Nikhil Nakod <40464766+nikhilsn45@users.noreply.github.com>
2021-08-18 09:24:28 +0530

    Update ClinicApplication.java

--- src/main/java/com/persistent/ClinicApplication.java
+++ src/main/java/com/persistent/ClinicApplication.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Online Doctor Recommendation", version = "1.0",description = "A web application for doctor recommendation."))
+@OpenAPIDefinition(info = @Info(title = "Online Appointment Booking", version = "1.0",description = "A web application for an appointment booking with Doctor"))
 public class ClinicApplication {
 
 	public static void main(String[] args) {
